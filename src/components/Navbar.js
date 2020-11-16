2e8b1e8900e09392b007212d9b4b402c1f84dfdb
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-11-17 04:47:40 +0500

    update font awsome link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
@@ -7,7 +7,7 @@ function Navbar() {
             <nav className='navbar'>
                 <div className='navbar-container'>
                     <Link to='/' className='navbar-logo'>
-                        TRVL <i className="fab fa-affiliatetheme"></i>
+                        TRVL <i className="fab fa-typo3"></i>
                     </Link>
                 </div>
             </nav>
