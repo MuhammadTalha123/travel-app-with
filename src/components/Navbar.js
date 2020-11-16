81ba1a6163423a86bebabc2e6b99b187710c3a08
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-11-16 06:04:01 +0500

    add font awsome link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
         <>
             <nav className='navbar'>
-                <div className='navbar-container'></div>
+                <div className='navbar-container'>
+                    <Link to='/' className='navbar-logo'>
+                        TRVL
+                    </Link>
+                </div>
             </nav>
         </>
     )
