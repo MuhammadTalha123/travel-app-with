deb4ba0f3c8a551e845553d7fba19290c976e6cc
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-11-19 03:39:05 +0500

    add cards in sign up page

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import '../../App.css';
+import Cards from '../Cards';
 
 export default function SignUp() {
-  return <h1 className='sign-up'>LIKE & SUBSCRIBE</h1>;
+  return (<> 
+  <h1 className='sign-up'>LIKE & SUBSCRIBE</h1>
+  <Cards/>
+  </>);
 }
