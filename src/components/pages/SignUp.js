23ddfd07e4db517bb9c82e46d7435ead6b7a6068
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-11-19 04:41:22 +0500

    adding form

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,29 +1,18 @@
 import React from 'react';
 import '../../App.css';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
 
 
 export default function SignUp() {
   return <>
   <div className='sign-up'>
   <h1 >LIKE & SUBSCRIBE</h1>
-  <Form className="form-submit">
-  <Form.Group controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label><br/>
-    <Form.Control type="email" placeholder="Enter email" /><br/>
-    <br/>
-  </Form.Group>
-
-  <Form.Group controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <br/>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form>
+  <form>
+    <label>Email</label><br/>
+    <input type='email' placeholder='Enter your Email' /><br/>
+    <label>Password</label><br/>
+    <input type='password' placeholder="Enter your Password" /><br/>
+    <input className='submitbtn' type='submit' />
+  </form>
   </div>
   </>
 }
