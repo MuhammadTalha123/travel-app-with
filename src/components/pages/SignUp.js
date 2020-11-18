4461959d2ae4f8797013f21a2267c820e1eb3fa7
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-11-19 03:43:07 +0500

    delete cards from signup page

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import '../../App.css';
-import Cards from '../Cards';
 
 export default function SignUp() {
-  return (<> 
-  <h1 className='sign-up'>LIKE & SUBSCRIBE</h1>
-  <Cards/>
-  </>);
+  return <h1 className='sign-up'>LIKE & SUBSCRIBE</h1>;
 }
