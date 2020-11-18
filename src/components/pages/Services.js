13d63df5fe532b5c099afffa833e1e772be3e958
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-11-19 03:31:25 +0500

    add cards in services

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import '../../App.css';
+import Cards from '../Cards'
 
 export default function Services() {
-  return <h1 className='services'>SERVICES</h1>;
+  return <>
+  <div className='servise-container'>
+  <h1 className='services'>SERVICES</h1>
+  <p className='serviceses'>we are provide best servies in our country</p>
+  <Cards/>
+  </div>
+  </>;
+
 }
