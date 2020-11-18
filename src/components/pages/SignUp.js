3934e81cbf3e60886841fec6bad4a1f2d391b1f4
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-11-19 04:09:58 +0500

    update form in signup

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import '../../App.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 
 export default function SignUp() {
-  return <h1 className='sign-up'>LIKE & SUBSCRIBE</h1>;
+  return <>
+  <div className='sign-up'>
+  <h1 >LIKE & SUBSCRIBE</h1>
+  <Form className="form-submit">
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label><br/>
+    <Form.Control type="email" placeholder="Enter email" /><br/>
+    <br/>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <br/>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+  </div>
+  </>
 }
